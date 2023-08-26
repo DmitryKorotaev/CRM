eb9d52f94d33f1da574113eb654c67c2470bbf5b
DmitryKorotaev <dm.kor98@mail.ru>
2023-08-26 17:27:46 +0300

    Проверка ввода- Vuelidate -- Login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import 'materialize-css/dist/js/materialize.min.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min.js";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
