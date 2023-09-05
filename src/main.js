1e625bd7441059f515725dda1993abf0d00020cb
DmitryKorotaev <dm.kor98@mail.ru>
2023-09-05 19:54:32 +0300

    --Plugin-- Исправить ошибку!!

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import MessagePlugin from "./utils/Message.plugin";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min.js";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(MessagePlugin).mount("#app");
---
+++ src/utils/Message.plugin.js
@@ -0,0 +1,12 @@
+export default {
+    install(app, options) {
+        Vue.config.globalProperties.$message 
+            M.toast({html}
+            )
+        Vue.config.globalProperties.$message = fanction(html) {
+            M.toast({html:`[Ошибка] ${html}`})
+        }
+
+
+    }
+}
