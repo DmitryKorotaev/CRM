8cab6d412477e05d6ba0e5b0bfdc445b92c01053
DmitryKorotaev <dm.kor98@mail.ru>
2023-09-08 19:06:38 +0300

    Firebase: установленна версия 8.1.2

--- src/main.js
+++ src/main.js
@@ -1,24 +1,31 @@
 import { createApp } from "vue";
-import { getAnalytics } from "firebase/analytics"
-import { initializeApp } from "firebase/app";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import MessagePlugin from "./utils/Message.plugin";
 import "./registerServiceWorker";
 import "materialize-css/dist/js/materialize.min.js";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBbFxAI6t3GvXHEJ-aNtA-XxFHkuQweylc",
-    authDomain: "vue-crm-183a1.firebaseapp.com",
-    projectId: "vue-crm-183a1",
-    storageBucket: "vue-crm-183a1.appspot.com",
-    messagingSenderId: "624685327614",
-    appId: "1:624685327614:web:d7b726e039abf9798571af",
-    measurementId: "G-2SYB45TZHM"
-   };
-   // Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBbFxAI6t3GvXHEJ-aNtA-XxFHkuQweylc",
+  authDomain: "vue-crm-183a1.firebaseapp.com",
+  projectId: "vue-crm-183a1",
+  storageBucket: "vue-crm-183a1.appspot.com",
+  messagingSenderId: "624685327614",
+  appId: "1:624685327614:web:d7b726e039abf9798571af",
+  measurementId: "G-2SYB45TZHM",
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    createApp(App).use(store).use(router).use(MessagePlugin).mount("#app");
+  }
+});
+// Initialize Firebase
 //const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
-
-createApp(App).use(store).use(router).use(MessagePlugin).mount("#app");
